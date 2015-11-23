7f30f9c044a9281bc346962e524e9a7045c8e2b1
duyetpt <duyetpt191993@gmail.com>
2015-11-23 15:34:48 +0700

    Fix bug error when chang 'đ' to 'd'

--- UtilsModule/src/main/java/org/Unicode.java
+++ UtilsModule/src/main/java/org/Unicode.java
@@ -1,26 +1,27 @@
 package org;
 
-import java.io.UnsupportedEncodingException;
 import java.text.Normalizer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.regex.Pattern;
 
 public class Unicode {
-	
-	private static Logger	logger	= LoggerFactory.getLogger(Unicode.class);
-	
-	public static String toAscii(String value) {
-		String str = Normalizer.normalize(value, Normalizer.Form.NFKD);
-		String regex = "[\\p{InCombiningDiacriticalMarks}}]+";
-		
-		String result = null;
-		try {
-			result = new String(str.replaceAll(regex, "").getBytes("ascii"), "ascii");
-		} catch (UnsupportedEncodingException e) {
-			logger.error("can't convert String: " + value, e);
-		}
-		
-		return result;
-	}
+
+//    private static final String VIETNAMESE_DIACRITIC_CHARACTERS
+//            = "ẮẰẲẴẶĂẤẦẨẪẬÂÁÀÃẢẠĐẾỀỂỄỆÊÉÈẺẼẸÍÌỈĨỊỐỒỔỖỘÔỚỜỞỠỢƠÓÒÕỎỌỨỪỬỮỰƯÚÙỦŨỤÝỲỶỸỴ";
+    public static String toAscii(String value) {
+        value = value.replace('đ', 'd');
+        String str = Normalizer.normalize(value.toLowerCase().trim(), Normalizer.Form.NFD);
+        String regex = "\\p{InCombiningDiacriticalMarks}+";
+
+        Pattern pattern = Pattern.compile(regex);
+        String result = pattern.matcher(str).replaceAll("");
+
+        return result.trim();
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+//         for (char c: VIETNAMESE_DIACRITIC_CHARACTERS.toCharArray()) {
+//         System.out.println(c + ": " + Character.getName(c));
+//         }
+        System.out.println(toAscii("xin chào Việt Nam. Món đậu phụ rất ngon"));
+    }
 }
