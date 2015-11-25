7dcc9768eed38bae60579ce94d83c272bb15af7b
duyetpt <duyetpt191993@gmail.com>
2015-11-25 23:19:19 +0700

    Fix bug setting language

--- DailyCookApp/src/main/java/com/vn/dailycookapp/notification/NotificationWorker.java
+++ DailyCookApp/src/main/java/com/vn/dailycookapp/notification/NotificationWorker.java
@@ -16,137 +16,138 @@ import com.vn.dailycookapp.cache.user.CompactUserInfo;
 import com.vn.dailycookapp.cache.user.UserCache;
 
 public class NotificationWorker extends Thread {
-	Logger	logger	= LoggerFactory.getLogger(getClass());
-	
-	public void run() {
-		while (true) {
-			logger.info("Notificaion worker is running...");
-			try {
-				Notification noti = NotificationActionImp.getInstance().getNoti();
-				// increate notification number
-				CompactUserInfo toUser = UserCache.getInstance().get(noti.getTo());
-				toUser.increaseNumberNotificaion();
-				UserDAO.getInstance().increateNotificationNumber(toUser.getUserId());
-				
-				List<Notification> list = null;
-				switch (noti.getType()) {
-					case Notification.NEW_COMMENT_TYPE:
-						list = notiNewComment(noti);
-						break;
-					case Notification.NEW_FAVORITE_TYPE:
-						list = notiFavorite(noti);
-						break;
-					case Notification.NEW_FOLLOWER_TYPE:
-						list = notiFollower(noti);
-						break;
-					case Notification.NEW_RECIPE_FROM_FOLLOWING_TYPE:
-						list = notiNewRecipe(noti);
-						break;
-					case Notification.WARM_TYPE:
-						// TODO
-						break;
-				}
-				
-				// save to DB
-				NotificationDAO.getInstance().save(list);
-			} catch (Exception e) {
-				logger.error("notification process exceiption", e);
-			}
-			
-		}
-	}
-	
-	private List<Notification> notiNewRecipe(Notification noti) {
-		CompactUserInfo user = null;
-		Following follow = null;
-		try {
-			user = UserCache.getInstance().get(noti.getFrom());
-			follow = FollowingDAO.getInstance().get(noti.getFrom(), Following.class);
-		} catch (DAOException e) {
-			logger.error("process notiNewRecipe error", e);
-		}
-		
-		List<Notification> notis = new ArrayList<Notification>();
-		for (String follower : follow.getFollowers()) {
-			Notification notiComp = new Notification();
-			notiComp.setFrom(noti.getFrom());
-			notiComp.setTo(follower);
-			notiComp.setRecipeId(noti.getRecipeId());
-			notiComp.setType(Notification.NEW_RECIPE_FROM_FOLLOWING_TYPE);
-			notiComp.setFromAvatar(user.getAvatarUrl());
-			notiComp.setFromName(user.getDisplayName());
-			notiComp.setRecipeTitle(noti.getRecipeTitle());
-			
-			notis.add(noti);
-		}
-		
-		return notis;
-	}
-	
-	private List<Notification> notiNewComment(Notification notification) {
-		CompactUserInfo user = null;
-		try {
-			user = UserCache.getInstance().get(notification.getFrom());
-			
-		} catch (DAOException e) {
-			logger.error("process notiNewComment error", e);
-		}
-		
-		Notification noti = new Notification();
-		noti.setFrom(notification.getFrom());
-		noti.setTo(notification.getTo());
-		noti.setRecipeId(notification.getRecipeId());
-		noti.setType(Notification.NEW_COMMENT_TYPE);
-		noti.setFromAvatar(user.getAvatarUrl());
-		noti.setFromName(user.getDisplayName());
-		noti.setRecipeTitle(notification.getRecipeTitle());
-		
-		List<Notification> notis = new ArrayList<Notification>();
-		notis.add(noti);
-		return notis;
-	}
-	
-	private List<Notification> notiFavorite(Notification notification) {
-		CompactUserInfo user = null;
-		try {
-			user = UserCache.getInstance().get(notification.getFrom());
-			
-		} catch (DAOException e) {
-			logger.error("process notiFavorite error", e);
-		}
-		
-		Notification noti = new Notification();
-		noti.setFrom(notification.getFrom());
-		noti.setTo(notification.getTo());
-		noti.setRecipeId(notification.getRecipeId());
-		noti.setType(Notification.NEW_FAVORITE_TYPE);
-		noti.setFromAvatar(user.getAvatarUrl());
-		noti.setFromName(user.getDisplayName());
-		noti.setRecipeTitle(notification.getRecipeTitle());
-		
-		List<Notification> notis = new ArrayList<Notification>();
-		notis.add(noti);
-		return notis;
-	}
-	
-	private List<Notification> notiFollower(Notification notification) {
-		CompactUserInfo user = null;
-		try {
-			user = UserCache.getInstance().get(notification.getFrom());
-			
-		} catch (DAOException e) {
-			logger.error("process notiFollower error", e);
-		}
-		
-		Notification noti = new Notification();
-		noti.setFrom(notification.getFrom());
-		noti.setTo(notification.getTo());
-		noti.setType(Notification.NEW_FOLLOWER_TYPE);
-		noti.setFromAvatar(user.getAvatarUrl());
-		noti.setFromName(user.getDisplayName());
-		
-		List<Notification> notis = new ArrayList<Notification>();
-		notis.add(noti);
-		return notis;
-	}
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void run() {
+        while (true) {
+            logger.info("Notificaion worker is running...");
+            try {
+                Notification noti = NotificationActionImp.getInstance().getNoti();
+                // increate notification number
+                CompactUserInfo toUser = UserCache.getInstance().get(noti.getTo());
+                toUser.increaseNumberNotificaion();
+                UserDAO.getInstance().increateNotificationNumber(toUser.getUserId());
+
+                List<Notification> list = null;
+                switch (noti.getType()) {
+                    case Notification.NEW_COMMENT_TYPE:
+                        list = notiNewComment(noti);
+                        break;
+                    case Notification.NEW_FAVORITE_TYPE:
+                        list = notiFavorite(noti);
+                        break;
+                    case Notification.NEW_FOLLOWER_TYPE:
+                        list = notiFollower(noti);
+                        break;
+                    case Notification.NEW_RECIPE_FROM_FOLLOWING_TYPE:
+                        list = notiNewRecipe(noti);
+                        break;
+                    case Notification.WARM_TYPE:
+                        // TODO
+                        break;
+                }
+
+                // save to DB
+                NotificationDAO.getInstance().save(list);
+            } catch (Exception e) {
+                logger.error("notification process exceiption", e);
+            }
+
+        }
+    }
+
+    private List<Notification> notiNewRecipe(Notification noti) {
+        CompactUserInfo user = null;
+        Following follow = null;
+        try {
+            user = UserCache.getInstance().get(noti.getFrom());
+            follow = FollowingDAO.getInstance().get(noti.getFrom(), Following.class);
+        } catch (DAOException e) {
+            logger.error("process notiNewRecipe error", e);
+        }
+
+        List<Notification> notis = new ArrayList<Notification>();
+        for (String follower : follow.getFollowers()) {
+            Notification notiComp = new Notification();
+            notiComp.setFrom(noti.getFrom());
+            notiComp.setTo(follower);
+            notiComp.setRecipeId(noti.getRecipeId());
+            notiComp.setType(Notification.NEW_RECIPE_FROM_FOLLOWING_TYPE);
+            notiComp.setFromAvatar(user.getAvatarUrl());
+            notiComp.setFromName(user.getDisplayName());
+            notiComp.setRecipeTitle(noti.getRecipeTitle());
+
+            notis.add(noti);
+        }
+
+        return notis;
+    }
+
+    private List<Notification> notiNewComment(Notification notification) {
+        CompactUserInfo user = null;
+        try {
+            user = UserCache.getInstance().get(notification.getFrom());
+
+        } catch (DAOException e) {
+            logger.error("process notiNewComment error", e);
+        }
+
+        Notification noti = new Notification();
+        noti.setFrom(notification.getFrom());
+        noti.setTo(notification.getTo());
+        noti.setRecipeId(notification.getRecipeId());
+        noti.setType(Notification.NEW_COMMENT_TYPE);
+        noti.setFromAvatar(user.getAvatarUrl());
+        noti.setFromName(user.getDisplayName());
+        noti.setRecipeTitle(notification.getRecipeTitle());
+
+        List<Notification> notis = new ArrayList<Notification>();
+        notis.add(noti);
+        return notis;
+    }
+
+    private List<Notification> notiFavorite(Notification notification) {
+        CompactUserInfo user = null;
+        try {
+            user = UserCache.getInstance().get(notification.getFrom());
+
+        } catch (DAOException e) {
+            logger.error("process notiFavorite error", e);
+        }
+
+        Notification noti = new Notification();
+        noti.setFrom(notification.getFrom());
+        noti.setTo(notification.getTo());
+        noti.setRecipeId(notification.getRecipeId());
+        noti.setType(Notification.NEW_FAVORITE_TYPE);
+        noti.setFromAvatar(user.getAvatarUrl());
+        noti.setFromName(user.getDisplayName());
+        noti.setRecipeTitle(notification.getRecipeTitle());
+
+        List<Notification> notis = new ArrayList<Notification>();
+        notis.add(noti);
+        return notis;
+    }
+
+    private List<Notification> notiFollower(Notification notification) {
+        CompactUserInfo user = null;
+        try {
+            user = UserCache.getInstance().get(notification.getFrom());
+
+        } catch (DAOException e) {
+            logger.error("process notiFollower error", e);
+        }
+
+        Notification noti = new Notification();
+        noti.setFrom(notification.getFrom());
+        noti.setTo(notification.getTo());
+        noti.setType(Notification.NEW_FOLLOWER_TYPE);
+        noti.setFromAvatar(user.getAvatarUrl());
+        noti.setFromName(user.getDisplayName());
+
+        List<Notification> notis = new ArrayList<Notification>();
+        notis.add(noti);
+        return notis;
+    }
 }
