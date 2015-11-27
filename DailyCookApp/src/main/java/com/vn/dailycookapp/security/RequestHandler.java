ac1346c615d31f29c60e63c38e0436a4c1459d15
duyetpt <duyetpt191993@gmail.com>
2015-11-27 12:32:20 +0700

    Fix bug token=anonymous

--- DailyCookApp/src/main/java/com/vn/dailycookapp/security/RequestHandler.java
+++ DailyCookApp/src/main/java/com/vn/dailycookapp/security/RequestHandler.java
@@ -24,51 +24,52 @@ import com.vn.dailycookapp.service.HeaderField;
 @Provider
 @Priority(Priorities.AUTHORIZATION)
 public class RequestHandler implements ContainerRequestFilter {
-	
-	private final Logger	logger	= LoggerFactory.getLogger(RequestHandler.class);
-	
-	public void filter(ContainerRequestContext requestContext) throws IOException {
-		requestContext.getHeaders().add("Accept", "*/*");
-		logger.info("			=====================  Start reqeust  =====================			");
-		logger.info("HTTP-METHOD: " + requestContext.getMethod());
-		MultivaluedMap<String, String> headers = requestContext.getHeaders();
-		JSONObject json = new JSONObject(headers);
-		logger.info("=> => => Request header => " + json.toString());
-		
-		String url = requestContext.getUriInfo().getPath();
-		logger.info("=> => => Request Url => " + url);
-		
+
+    private final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        requestContext.getHeaders().add("Accept", "*/*");
+        logger.info("			=====================  Start reqeust  =====================			");
+        logger.info("HTTP-METHOD: " + requestContext.getMethod());
+        MultivaluedMap<String, String> headers = requestContext.getHeaders();
+        JSONObject json = new JSONObject(headers);
+        logger.info("=> => => Request header => " + json.toString());
+
+        String url = requestContext.getUriInfo().getPath();
+        logger.info("=> => => Request Url => " + url);
+
 		// String query =
-		// requestContext.getUriInfo().getRequestUri().getQuery();
-		
+        // requestContext.getUriInfo().getRequestUri().getQuery();
 		// if (query != null && query.endsWith("testMode=true")) {
-		// logger.info("run in test mode...");
-		// } else {
-		String token = requestContext.getHeaderString(HeaderField.TOKEN);
-		String userId = null;
-		if (NotAuthUrls.isNotAuth(url)) {
-			logger.info("don't need auth for this url...");
-			if (token != null) {
-				try {
-					userId = Authorizer.getInstance().authorize(token);
-                                        requestContext.getHeaders().putSingle(HeaderField.USER_ID, userId);
-				} catch (AuthorizationException e) {
-					logger.error("authorzation exception", e);
-				}
-			}
-		} else {
-			logger.info("authorzation...");
-			try {
-				userId = Authorizer.getInstance().authorize(token);
-				requestContext.getHeaders().add(HeaderField.USER_ID, userId);
-			} catch (AuthorizationException e) {
-				logger.error("authorzation exception", e);
-				DCAResponse response = new DCAResponse(e.getErrorCode());
-				requestContext.abortWith(Response.ok(JsonTransformer.getInstance().marshall(response)).build());
-			}
-		}
+        // logger.info("run in test mode...");
+        // } else {
+        String token = requestContext.getHeaderString(HeaderField.TOKEN);
+        String userId = null;
+        if (NotAuthUrls.isNotAuth(url)) {
+            logger.info("don't need auth for this url...");
+            if (token != null) {
+                try {
+                    if (!token.equals("anonymous")) {
+                        userId = Authorizer.getInstance().authorize(token);
+                        requestContext.getHeaders().putSingle(HeaderField.USER_ID, userId);
+                    }
+                } catch (AuthorizationException e) {
+                    logger.error("authorzation exception", e);
+                }
+            }
+        } else {
+            logger.info("authorzation...");
+            try {
+                userId = Authorizer.getInstance().authorize(token);
+                requestContext.getHeaders().add(HeaderField.USER_ID, userId);
+            } catch (AuthorizationException e) {
+                logger.error("authorzation exception", e);
+                DCAResponse response = new DCAResponse(e.getErrorCode());
+                requestContext.abortWith(Response.ok(JsonTransformer.getInstance().marshall(response)).build());
+            }
+        }
 		// }
-		
-	}
-	
+
+    }
+
 }
