f12a0bb2f3ea93516e6246abd1ef7ed24e11a817
duyetpt <duyetpt191993@gmail.com>
2015-11-29 20:37:00 +0700

    Fix bug recipeId=null when noti create new recipe

--- DailyCookApp/src/main/java/com/vn/dailycookapp/restmodel/model/GetRecipeModel.java
+++ DailyCookApp/src/main/java/com/vn/dailycookapp/restmodel/model/GetRecipeModel.java
@@ -15,52 +15,48 @@ import com.vn.dailycookapp.restmodel.InvalidParamException;
 import com.vn.dailycookapp.utils.ErrorCodeConstant;
 
 /**
- * 
- * @author duyetpt
- *         Check recipe id
- *         Get recipe by id
- *         Get owner information
- *         Do user favour this recipe
- *         Do user following this owner
+ *
+ * @author duyetpt Check recipe id Get recipe by id Get owner information Do
+ * user favour this recipe Do user following this owner
  */
 public class GetRecipeModel extends AbstractModel {
-	
-	@Override
-	protected void preExecute(String... data) throws Exception {
-		try {
-			myId = data[0];
-			recipeId = data[1];
-		} catch (Exception ex) {
-			throw new InvalidParamException();
-		}
-		validateData();
-		
-	}
-	
-	@Override
-	protected DCAResponse execute() throws Exception {
-		DCAResponse response = new DCAResponse(ErrorCodeConstant.SUCCESSUL.getErrorCode());
-		Recipe recipe = RecipeDAO.getInstance().get(recipeId);
-		CompactUserInfo owner = UserCache.getInstance().get(recipe.getOwner());
-		
-		// check user favorite recipe
-		recipe.setIsFavorite(myId == null ? false : FavoriteDAO.getInstance().isFavorite(myId, recipeId));
-		// check userid is following owner of recipe
-		boolean isFollowingOwner = myId == null ? false : FollowingDAO.getInstance().isFollowing(myId, owner.getUserId());
-		
-		RecipeResponseData data = new RecipeResponseData(recipe, owner);
-		data.setIsFollowing(isFollowingOwner);
-		
-		// Increase view number
-		RecipeDAO.getInstance().increateView(recipeId);
-		
-		response.setData(data);
-		return response;
-	}
-	
-	private void validateData() throws InvalidParamException {
-		if (!ObjectId.isValid(recipeId)) {
-			throw new InvalidParamException();
-		}
-	}
+
+    @Override
+    protected void preExecute(String... data) throws Exception {
+        try {
+            myId = data[0];
+            recipeId = data[1];
+        } catch (Exception ex) {
+            throw new InvalidParamException();
+        }
+        validateData();
+
+    }
+
+    @Override
+    protected DCAResponse execute() throws Exception {
+        DCAResponse response = new DCAResponse(ErrorCodeConstant.SUCCESSUL.getErrorCode());
+        Recipe recipe = RecipeDAO.getInstance().get(recipeId);
+        CompactUserInfo owner = UserCache.getInstance().get(recipe.getOwner());
+
+        // check user favorite recipe
+        recipe.setIsFavorite(myId == null ? false : FavoriteDAO.getInstance().isFavorite(myId, recipeId));
+        // check userid is following owner of recipe
+        boolean isFollowingOwner = myId == null ? false : FollowingDAO.getInstance().isFollowing(myId, owner.getUserId());
+
+        RecipeResponseData data = new RecipeResponseData(recipe, owner);
+        data.setIsFollowing(isFollowingOwner);
+
+        // Increase view number
+        RecipeDAO.getInstance().increateView(recipeId);
+
+        response.setData(data);
+        return response;
+    }
+
+    private void validateData() throws InvalidParamException {
+        if (!ObjectId.isValid(recipeId)) {
+            throw new InvalidParamException();
+        }
+    }
 }
