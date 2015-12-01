9a3089cce2fd6bbd785b8cee1c7fcf8de18d4dae
nguyennam2911 <namnhse02675@fpt.edu.vn>
2015-12-01 13:38:36 +0700

    Revert "recipe"
    This reverts commit 79c0c0878e95b14179aa4f74c2222fc95ba338ac.

--- UtilsModule/src/main/java/org/dao/RecipeDAO.java
+++ UtilsModule/src/main/java/org/dao/RecipeDAO.java
@@ -232,45 +232,42 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class).filter("status_flag", Recipe.APPROVED_FLAG);
             query.field("normalize_title").contains(Unicode.toAscii(name)).offset(skip).limit(take);
-            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time", "favorite_number");
-
+            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time" ,"favorite_number");
+            
             return query.asList();
         } catch (Exception ex) {
             throw new DAOException();
         }
     }
-
-    public List<Recipe> searchAllAndFilterRecipeByName(String name, int skip, int take, int flag, String Oder) throws DAOException {
+    public List<Recipe> searchAllAndFilterRecipeByName(String name, int skip, int take,int flag,String Oder) throws DAOException {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class).filter("status_flag", flag);
             query.field("normalize_title").containsIgnoreCase(Unicode.toAscii(name)).offset(skip).limit(take);
-            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time", "favorite_number").order(Oder);
+            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time","favorite_number").order(Oder);
             return query.asList();
         } catch (Exception ex) {
             throw new DAOException();
         }
     }
-
     public List<Recipe> searchAllRecipeByName(String name, int skip, int take, String order) throws DAOException {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class);
             query.field("normalize_title").containsIgnoreCase(Unicode.toAscii(name)).offset(skip).limit(take);
-            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time", "favorite_number").order(order);
-
+            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time","favorite_number").order(order);
+            
             return query.asList();
         } catch (Exception ex) {
             throw new DAOException();
         }
     }
-
+    
     /**
      * count number recipe match name
-     *
      * @param name
      * @param skip
      * @param take
      * @return
-     * @throws DAOException
+     * @throws DAOException 
      */
     public long getSearchResultNumber(String name) throws DAOException {
         try {
@@ -281,8 +278,7 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
             throw new DAOException();
         }
     }
-
-    public long getSearchAndFilterResultNumber(String name, int flag_active) throws DAOException {
+    public long getSearchAndFilterResultNumber(String name,int flag_active) throws DAOException {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class).filter("status_flag", flag_active);
             query.field("normalize_title").contains(Unicode.toAscii(name));
@@ -291,7 +287,6 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
             throw new DAOException();
         }
     }
-
     public long getSearchAllResultNumber(String name) throws DAOException {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class);
@@ -301,12 +296,12 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
             throw new DAOException();
         }
     }
-
+    
     public List<Recipe> getAllRecipe() {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class);
             query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time");
-
+            
             return query.asList();
         } catch (Exception ex) {
             logger.error("getAllRecipe fail", ex);
@@ -318,7 +313,7 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
     public List<Recipe> getRecipes(int skip, int take) {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class).offset(skip).limit(take);
-            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time", "favorite_number");
+            query.retrievedFields(true, "picture_url", "status_flag", "owner", "title", "created_time","favorite_number");
             return query.asList();
         } catch (Exception ex) {
             logger.error("getAllRecipe fail", ex);
@@ -326,7 +321,7 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
 
         return null;
     }
-
+    
     // for webservice when user report recipe
     public boolean updateRecipeStatus(String recipeId, int flag) {
         try {
@@ -359,9 +354,10 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
             Query<Recipe> query = datastore.createQuery(Recipe.class).field("_id").equal(new ObjectId(recipeId));
             return query.get();
         } catch (Exception ex) {
-            ex.printStackTrace();
-            return null;
+
         }
+
+        return null;
     }
 
     // count number recipe create in period
@@ -383,26 +379,26 @@ public class RecipeDAO extends AbstractDAO<Recipe> {
         Query<Recipe> query = datastore.createQuery(Recipe.class);
         return query.countAll();
     }
-
+    
     // get recipe of user
     public List<Recipe> getRecipeOfUser(String userId, int skip, int take) throws DAOException {
         try {
             Query<Recipe> query = datastore.createQuery(Recipe.class).filter("status_flag", Recipe.APPROVED_FLAG).filter("owner", userId).limit(take).offset(skip).order("-created_time");
-
+            
             return query.asList();
         } catch (Exception ex) {
             logger.error("get recipe of user error:" + userId, ex);
             throw new DAOException();
         }
     }
-
+    
     // count number recipe with flag
     public long getNumberRecipeWithFlag(int flag) {
         Query<Recipe> query = datastore.createQuery(Recipe.class);
         query.and(query.criteria("status_flag").equal(flag));
         return query.countAll();
     }
-
+    
     //get all recipes of user
     public List<Recipe> getRecipeOfUser(String userId) throws DAOException {
         try {
