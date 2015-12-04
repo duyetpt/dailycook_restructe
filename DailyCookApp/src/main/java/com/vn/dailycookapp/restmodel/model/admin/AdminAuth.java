7d6f9276a4a3860d1a5bfaaa77483f7306d9b6af
duyetpt <duyetpt191993@gmail.com>
2015-12-04 12:09:50 +0700

    encrypt password of admin

--- DailyCookApp/src/main/java/com/vn/dailycookapp/restmodel/model/admin/AdminAuth.java
+++ DailyCookApp/src/main/java/com/vn/dailycookapp/restmodel/model/admin/AdminAuth.java
@@ -8,6 +8,8 @@ package com.vn.dailycookapp.restmodel.model.admin;
 import com.vn.dailycookapp.entity.request.RegisterInfo;
 import com.vn.dailycookapp.security.authentication.LoginFailException;
 import com.vn.dailycookapp.utils.ErrorCodeConstant;
+import org.EncryptDataException;
+import org.EncryptHelper;
 import org.dao.DAOException;
 import org.dao.UserDAO;
 import org.entity.User;
@@ -18,12 +20,12 @@ import org.entity.User;
  */
 public class AdminAuth {
 
-    static void auth(RegisterInfo adminAcc) throws LoginFailException, DAOException {
+    static void auth(RegisterInfo adminAcc) throws LoginFailException, DAOException, EncryptDataException {
         User user = UserDAO.getInstance().getUserInfoByEmail(adminAcc.getEmail());
         if (user == null) {
             throw new LoginFailException(ErrorCodeConstant.USER_NOT_FOUND);
 //        } else if (!user.getPassword().equals(EncryptHelper.encrypt(adminAcc.getPassword()))) {
-        } else if (!user.getPassword().equals(adminAcc.getPassword()) || user.getRole().equals(User.NORMAL_USER_ROLE)) {
+        } else if (!user.getPassword().equals(EncryptHelper.encrypt(adminAcc.getPassword())) || user.getRole().equals(User.NORMAL_USER_ROLE)) {
             throw new LoginFailException(ErrorCodeConstant.PASSWORD_INCORRECT);
         }
     }
