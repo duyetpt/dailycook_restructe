5b38f058fcbd7be53f56df6f90a8b01370094ed7
dongockhanh <khanh.dongoc93@gmail.com>
2015-12-06 09:34:52 +0700

    sort by time statistic user

--- UtilsModule/src/main/java/org/dao/ActivityLogDAO.java
+++ UtilsModule/src/main/java/org/dao/ActivityLogDAO.java
@@ -12,6 +12,7 @@ import org.entity.ActivityLog.Count;
 import org.mongodb.morphia.aggregation.Accumulator;
 import org.mongodb.morphia.aggregation.AggregationPipeline;
 import org.mongodb.morphia.aggregation.Group;
+import org.mongodb.morphia.aggregation.Sort;
 import org.mongodb.morphia.query.Query;
 
 /**
@@ -46,7 +47,7 @@ public class ActivityLogDAO extends AbstractDAO<ActivityLog> {
         try {
             AggregationPipeline aggregation = datastore.createAggregation(ActivityLog.class);
             aggregation.match(datastore.createQuery(ActivityLog.class).filter("time >= ", from.getTime()).filter("time <=", to.getTime()));
-            Iterator<Count> result = aggregation.group("time", Group.grouping("count", new Accumulator("$sum", 1))).aggregate(Count.class);
+            Iterator<Count> result = aggregation.group("time", Group.grouping("count", new Accumulator("$sum", 1))).sort(Sort.ascending("time")).aggregate(Count.class);
 
             return result;
         } catch (Exception ex) {
